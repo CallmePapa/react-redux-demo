8fdba9520b515b375799c133a3df38591babf77b
CallmePapa <1062927638@qq.com>
2018-05-26 21:46:20 +0800

    modified

--- todos/src/containers/FilterLink.js
+++ todos/src/containers/FilterLink.js
@@ -1,18 +1,18 @@
-import {connect} from'react-redux';
+import {connect} from 'react-redux';
 import {setVisibilityFilter} from "../actions";
 import Link from '../components/Link';
 
-const mapStateToProps=(state,ownProps)=>({
-    active:ownProps.filter===state.visibilityFilter
+const mapStateToProps = (state, ownProps) => ({
+    active: ownProps.filter === state.visibilityFilter
 });
 
-const mapDispatchToProps=(dispatch,ownProps)=>({
-    onClick:()=>{
+const mapDispatchToProps = (dispatch, ownProps) => ({
+    onClick: () => {
         dispatch(setVisibilityFilter(ownProps.filter))
     }
 });
 
-const FilterLink=connect(
+const FilterLink = connect(
     mapStateToProps,
     mapDispatchToProps
 )(Link);
